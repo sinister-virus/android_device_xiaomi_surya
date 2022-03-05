bc22cb2ea7e70fcd3b3a62c3b0776896f217787b
#Pratham! <prathampb@hotmail.com>
2022-03-05 22:03:16 +0530

    Update twrp_surya.mk

--- twrp_surya.mk
+++ twrp_surya.mk
@@ -16,12 +16,6 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
